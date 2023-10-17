582730bcde0093c790d2d96777a9441fae77a612
VIGNESH VASU <125632985+vignesh2004vasu@users.noreply.github.com>
2023-10-17 18:11:07 +0530

    DAY_4_Rotate_Image.cpp

--- LeetCode/DAY_4_Rotate_Image.cpp
+++ LeetCode/DAY_4_Rotate_Image.cpp
@@ -1,1 +1,27 @@
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+DO NOT allocate another 2D matrix and do the rotation.
 
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Answer:
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        int n=matrix.size();
+        cout<<n;
+         for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    
+   
+    for (int i = 0; i < n; i++) {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+        
+    }
+};

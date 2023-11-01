62d67dfea8ec8e9b948d73e2581b2725d08d669a
VIGNESH VASU <125632985+vignesh2004vasu@users.noreply.github.com>
2023-11-01 21:34:53 +0530

    DAY_18_Bleak_Numbers.cpp

--- GeeksForGeeks/DAY_18_Bleak_Numbers.cpp
+++ GeeksForGeeks/DAY_18_Bleak_Numbers.cpp
@@ -1,7 +1,6 @@
 Given an integer, check whether it is Bleak or not.
 A number n is called Bleak if it cannot be represented as sum of a positive number x and set bit count in x,
 i.e., x + countSetBits(x) is not equal to n for any non-negative number x.
-
 Example 1:
 Input: 
 4

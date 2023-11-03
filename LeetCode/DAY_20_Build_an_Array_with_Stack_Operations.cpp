5ad82876be41f7c7e571ebd2f62845ad96601e66
VIGNESH VASU <125632985+vignesh2004vasu@users.noreply.github.com>
2023-11-03 11:49:34 +0530

    DAY_20_Build_an_Array_with_Stack_Operations.cpp

---
+++ LeetCode/DAY_20_Build_an_Array_with_Stack_Operations.cpp
@@ -0,0 +1,45 @@
+You are given an integer array target and an integer n.
+You have an empty stack with the two following operations:
+
+"Push": pushes an integer to the top of the stack.
+"Pop": removes the integer on the top of the stack.
+You also have a stream of the integers in the range [1, n].
+
+Use the two stack operations to make the numbers in the stack (from the bottom to the top) equal to target. You should follow the following rules:
+
+If the stream of the integers is not empty, pick the next integer from the stream and push it to the top of the stack.
+If the stack is not empty, pop the integer at the top of the stack.
+If, at any moment, the elements in the stack (from the bottom to the top) are equal to target, do not read new integers from the stream and do not do more operations on the stack.
+Return the stack operations needed to build target following the mentioned rules. If there are multiple valid answers, return any of them.
+
+Example 1:
+
+Input: target = [1,3], n = 3
+Output: ["Push","Push","Pop","Push"]
+Explanation: Initially the stack s is empty. The last element is the top of the stack.
+Read 1 from the stream and push it to the stack. s = [1].
+Read 2 from the stream and push it to the stack. s = [1,2].
+Pop the integer on the top of the stack. s = [1].
+Read 3 from the stream and push it to the stack. s = [1,3].
+Answer:
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+      vector<string> result;
+        int i = 1; 
+
+    for (int t : target) {
+        while (i < t) {
+
+            result.push_back("Push");
+            result.push_back("Pop");
+            i++;
+        }
+ 
+        result.push_back("Push");
+        i++;
+    }
+
+    return result;
+    }
+};
